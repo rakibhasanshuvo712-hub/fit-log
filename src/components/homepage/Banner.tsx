2e1e2e710d1b526f2fed21eabb907b733e0174c4
rakibhasanshuvo712-hub <rakibhasanshuvo712@gmail.com>
2026-09-24 10:00:49 -0600

    edited error

--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -15,8 +15,8 @@ into today's plan, and watch the week's work add up.</p>
             </div>
             <div>
            <Image src={bannerr}
- height={60} 
- width={50}
+ height={200} 
+ width={200}
   alt="Logo" />
             </div>
         </div>
